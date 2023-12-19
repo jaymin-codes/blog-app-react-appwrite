34dd46dc85d466dd85ac841afcf7c69c0461b930
jaymin-codes <jayminmistry1008@gmail.com>
2023-12-19 18:43:10 +0530

    first commit

--- blogApp_React_Appwrite/src/App.jsx
+++ blogApp_React_Appwrite/src/App.jsx
@@ -1,6 +1,10 @@
 import "./App.css";
 
 function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  console.log(import.meta.env.DB_PASSWORD);
+
   return (
     <>
       <h1>blog with appwrite</h1>
